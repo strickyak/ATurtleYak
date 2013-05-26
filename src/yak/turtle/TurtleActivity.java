181c720bd83af728dda2fcf2cb7094cde414eb19
Strick Yak <strickyak@gmail.com>
2013-05-26 02:05:05 -0700

    three logo commands: l, r, f.

--- src/yak/turtle/TurtleActivity.java
+++ src/yak/turtle/TurtleActivity.java
@@ -1,28 +1,42 @@
 package yak.turtle;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import yak.turtle.Views.AListView;
+import yak.turtle.Views.AListViewOfActivityUris;
 import yak.turtle.Views.ATextView;
+import yak.turtle.Views.AnEditView;
+import yak.turtle.Views.DrawView;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.LauncherActivity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 
 public class TurtleActivity extends Activity {
+	static HashMap<String, String> boxes = new HashMap<String, String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// setContentView(R.layout.activity_turtle);
-		
+
 		Intent intent = getIntent();
 		Uri uri = intent.getData();
 		Bundle extras = intent.getExtras();
-		String path = uri == null ? "/" : uri.getPath();
-		String query = uri == null ? "" : uri.getQuery();
-		
-		String text = "PATH " + path + " QUERY " + query + " INTENT " + intent + " EXTRAS " + extras;
-		ATextView tv = new ATextView(this, text);
-		setContentView(tv);
+		String pathStr = uri == null ? "/" : uri.getPath();
+		String queryStr = uri == null ? "" : uri.getQuery();
+
+		String[] path = pathStr.substring(1).split("/");
+		HashMap<String, String> query = parseQueryStr(queryStr);
+
+		dispatch(path, query);
+
 	}
 
 	@Override
@@ -30,5 +44,153 @@ public class TurtleActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.turtle, menu);
 		return true;
+	}
+
+	HashMap<String, String> parseQueryStr(String queryStr) {
+		HashMap<String, String> z = new HashMap<String, String>();
+		queryStr = queryStr == null ? "" : queryStr; // Don't be null.
+
+		String[] parts = queryStr.split("&");
+		for (String part : parts) {
+			String[] kv = part.split("=", 2);
+			if (kv.length == 2)
+				try {
+					z.put(kv[0], URLDecoder.decode(kv[1], "UTF-8"));
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+		}
+		return z;
+	}
+
+	void dispatch(String[] path, HashMap<String, String> query) {
+		Log.v("yak", "DISPATCH {" + Show(path) + "}" + path.length);
+		if (path.length == 0 || path[0].length() == 0) {
+			doRoot();
+		} else if (path.length == 1 && query.size() == 0) {
+			doBox(path[0]);
+		} else if (path.length == 2 && query.size() == 0
+				&& path[1].equals("RUN")) {
+			doRun(path[0]);
+		} else {
+			String text = "PATH " + path + " QUERY " + query;
+			ATextView tv = new ATextView(this, text);
+			setContentView(tv);
+		}
+	}
+
+	void doRoot() {
+		String[] labels = { "/A", "/B", "/C", "/D", "/E", "/F", "/G" };
+		AListView lv = new AListViewOfActivityUris(this, labels,
+				TurtleActivity.class);
+		setContentView(lv);
+	}
+
+	void doBox(final String box) {
+		String text = boxes.get(box);
+		if (text == null) {
+			text = "?" + box;
+		}
+		AnEditView e = new AnEditView(this, text) {
+			@Override
+			void onSave(String newText) {
+				boxes.put(box, newText);
+				Views.launchActivity(TurtleActivity.this, TurtleActivity.class,
+						"/" + box + "/" + "RUN", "");
+			}
+		};
+		setContentView(e);
+	}
+
+	void doRun(final String box) {
+		final String text = boxes.get(box);
+		ArrayList<Float> v = new ArrayList<Float>();
+		// for (int i = 0; i < 30; i++) {
+		// v.add((float)(i*4));
+		// v.add((float)(i*8));
+		// v.add((float)(100 - i*3));
+		// v.add((float)(100 - i*7));
+		// }
+		v = runLogo(parseLogo(text));
+		DrawView dv = new DrawView(this, v);
+		setContentView(dv);
+	}
+
+	ArrayList<Float> runLogo(String[] words) {
+		final int n = words.length;
+		ArrayList<Float> v = new ArrayList<Float>();
+		double x = 50; // turtle position
+		double y = 50;
+		double a = 0; // angle
+		for (int i = 0; i < n; i++) {
+			String w = words[i];
+			x = (x + 1000000) % 100.0;
+			y = (y + 1000000) % 100.0;
+			if (w.equals("f")) { // Forward
+				double d = parseDouble(words[i + 1]);
+				++i;
+				double xx = x + d * Math.cos(a / 180.0 * Math.PI);
+				double yy = y + d * Math.sin(a / 180.0 * Math.PI);
+				v.add((float) x);
+				v.add((float) y);
+				v.add((float) xx);
+				v.add((float) yy);
+				x = xx;
+				y = yy;
+			} else if (w.equals("l")) { // Left
+				double d = parseDouble(words[i + 1]);
+				++i;
+				a -= d;
+			} else if (w.equals("r")) { // right
+				double d = parseDouble(words[i + 1]);
+				++i;
+				a += d;
+			} else {
+				throw new RuntimeException("Unknown Command: {" + w + "}");
+			}
+		}
+		return v;
+	}
+
+	double parseDouble(String s) {
+		if (s.length() == 1 && 'a' <= s.charAt(0) && s.charAt(0) <= 'z') {
+			// Shortcut 'a' thru 'z' are 1 thru 26.
+			return s.charAt(0) - 'a' + 1;
+		}
+		return Double.parseDouble(s);
+	}
+
+	String[] parseLogo(String a) {
+		return removeEmptyStrings(a.split("[\\t\\n\\r ]+"));
+	}
+
+	String[] removeEmptyStrings(String[] a) {
+		final int n = a.length;
+		int c = 0;
+		for (int i = 0; i < n; i++) {
+			if (a[i].length() > 0) {
+				c++;
+			}
+		}
+		String[] z = new String[c];
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+			if (a[i].length() > 0) {
+				z[j] = a[i];
+				++j;
+			}
+		}
+		return z;
+	}
+
+	public static String Show(String[] ss) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("{arr ");
+		for (int i = 0; i < ss.length; i++) {
+			sb.append("[" + i + "]= " + ss[i] + " ");
+		}
+		sb.append("}");
+		return sb.toString();
 	}
 }
--- src/yak/turtle/Views.java
+++ src/yak/turtle/Views.java
@@ -1,26 +1,38 @@
 package yak.turtle;
 
 import java.net.URI;
+import java.util.ArrayList;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Build;
+import android.text.InputType;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.LinearLayout.LayoutParams;
 
 public final class Views {
+
 	public static abstract class AListView extends ListView {
-		private Context theContext;
+		protected Context theContext;
 
 		String[] labels;
 
@@ -44,10 +56,27 @@ public final class Views {
 					final String label = labels[index];
 					handleItemClick(label);
 				}
-				
+
 			});
 		}
+
 		public abstract void handleItemClick(String label);
+	}
+
+	public static class AListViewOfActivityUris extends AListView {
+		Class theClass;
+
+		public AListViewOfActivityUris(Context cx, String[] labels,
+				Class theClass) {
+			super(cx, labels);
+			this.theClass = theClass;
+		}
+
+		@Override
+		public void handleItemClick(String label) {
+			launchActivity(theContext, theClass, label, "");
+		}
+
 	}
 
 	public static abstract class AWebView extends WebView {
@@ -87,11 +116,13 @@ public final class Views {
 
 			return true;
 		}
+
 		protected abstract void handleClick(String path, String query);
 	}
 
 	public static class ATextView extends TextView {
 		private Context theContext;
+
 		public ATextView(Context cx, String text) {
 			super(cx);
 			this.theContext = cx;
@@ -105,6 +136,7 @@ public final class Views {
 
 	public static class AVerticalView extends LinearLayout {
 		private Context theContext;
+
 		public AVerticalView(Context cx) {
 			super(cx);
 			this.theContext = cx;
@@ -114,11 +146,111 @@ public final class Views {
 
 		@Override
 		public void addView(View view) {
-			Log.i("VerticalView", theContext.toString() + "=== addView: " + view);
+			Log.i("VerticalView", theContext.toString() + "=== addView: "
+					+ view);
 			super.addView(view);
 		}
 	}
 
-	public static LayoutParams FILL = new LayoutParams(LayoutParams.FILL_PARENT,
-			LayoutParams.FILL_PARENT);
+	public abstract static class AnEditView extends LinearLayout {
+
+		final private Context theContext;
+
+		final Button btn;
+		final EditText ed;
+		String text;
+
+		AnEditView(Context cx, String text) {
+			super(cx);
+			this.theContext = cx;
+			this.text = text;
+
+			ed = new EditText(theContext);
+
+			ed.setText(text);
+
+			ed.setInputType(InputType.TYPE_CLASS_TEXT
+					| InputType.TYPE_TEXT_FLAG_MULTI_LINE
+					| InputType.TYPE_TEXT_VARIATION_LONG_MESSAGE
+					| InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+			ed.setLayoutParams(FILL_1);
+			// ed.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+			ed.setTextAppearance(theContext, R.style.teletype);
+			ed.setBackgroundColor(Color.BLACK);
+			ed.setGravity(Gravity.TOP);
+			ed.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_INSET);
+			ed.setVerticalFadingEdgeEnabled(true);
+			ed.setVerticalScrollBarEnabled(true);
+			ed.setOnKeyListener(new OnKeyListener() {
+				public boolean onKey(View v, int keyCode, KeyEvent event) {
+					return false;
+				}
+			});
+
+			btn = new Button(theContext);
+			btn.setText("Save");
+			btn.setOnClickListener(new OnClickListener() {
+				public void onClick(View v) {
+					onSave(ed.getText().toString());
+				}
+			});
+
+			this.setOrientation(LinearLayout.VERTICAL);
+			this.addView(btn);
+			this.addView(ed);
+		}
+
+		abstract void onSave(String text);
+	}
+
+	public static class DrawView extends View {
+		ArrayList<Float> lines;
+
+		public DrawView(Context context, ArrayList<Float> lines) {
+			super(context);
+			this.lines = lines;
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas) {
+			Paint black = new Paint();
+			black.setColor(Color.BLACK);
+			canvas.drawPaint(black);
+
+			Paint green = new Paint();
+			green.setColor(Color.GREEN);
+			green.setStrokeWidth(4);
+
+			int w = canvas.getWidth();
+			int h = canvas.getHeight();
+			float ww = w / 100.0f;
+			float hh = h / 100.0f;
+
+			final int n = lines.size() / 4;
+			for (int i = 0; i < n; i++) {
+				float x1 = ww * lines.get(4 * i + 0);
+				float y1 = hh * lines.get(4 * i + 1);
+				float x2 = ww * lines.get(4 * i + 2);
+				float y2 = hh * lines.get(4 * i + 3);
+				canvas.drawLine(x1, y1, x2, y2, green);
+			}
+		}
+	}
+
+	public static LayoutParams FILL = new LayoutParams(
+			LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+
+	public static LayoutParams FILL_1 = new LayoutParams(
+			ViewGroup.LayoutParams.FILL_PARENT,
+			ViewGroup.LayoutParams.FILL_PARENT, 1.0f);
+
+	public static void launchActivity(Context theContext, Class theClass,
+			String path, String query) {
+		Log.v("yak", "LAUNCHING {" + path + "}");
+		Uri uri = new Uri.Builder().scheme("yak").path(path).query(query)
+				.build();
+		Intent intent = new Intent("android.intent.action.MAIN", uri);
+		intent.setClass(theContext.getApplicationContext(), theClass);
+		theContext.startActivity(intent);
+	}
 }
